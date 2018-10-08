4134297f7b51f260e14bb38885423698548e0ded
warnickb <warnickb@mail.gvsu.edu>
2018-10-08 11:48:29 -0400

    added basic GUI

---
+++ battleshipproject/src/battleshipproject/GUI.java
@@ -0,0 +1,60 @@
+package battleshipproject;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.FlowLayout;
+import java.awt.GridBagLayout;
+import javax.swing.BoxLayout;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.FormSpecs;
+import com.jgoodies.forms.layout.RowSpec;
+import java.awt.GridLayout;
+
+public class GUI extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUI frame = new GUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public GUI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new GridLayout(0, 3, 0, 0));
+		
+		JButton btnNewGame = new JButton("New Game");
+		contentPane.add(btnNewGame);
+		
+		JButton btnSettings = new JButton("Settings");
+		contentPane.add(btnSettings);
+		
+		JButton btnExit = new JButton("Exit");
+		contentPane.add(btnExit);
+	}
+
+}
